35f1eca28b804b1cca93a238af7566a7ee397033
Joeziojr <joeziojunior@gmail.com>
2023-06-30 10:50:50 -0300

    Verificação de existência de user por email e nome, separadamente

--- src/com/edu/ifba/main/Main.java
+++ src/com/edu/ifba/main/Main.java
@@ -29,7 +29,7 @@ public class Main extends Application {
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.setTitle("Pocotoro");
-        stage.setResizable(true);
+        stage.setResizable(false);
         stage.centerOnScreen();
         stage.show();
     }
